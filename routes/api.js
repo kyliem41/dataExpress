9f0d95522e82e60a0c33a479dc2335f1083b15b5
kyliem41 <krhodus@student.neumont.edu>
2023-08-31 01:08:43 -0600

    charts and api

--- routes/api.js
+++ routes/api.js
@@ -1,41 +1,47 @@
 var express = require('express');
-var session = require('express-session');
 var router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const uri = 'mongodb://localhost:2717';
-const client = new MongoClient(uri);
-const database = client.db('dataExpress');
-const collection = database.collection('users');
 
-/* GET home page. */
-router.get('/', async function(req, res, next) { // get graph data
+router.get('/', async function(req, res, next) {
+  const client = new MongoClient(uri);
 
-    const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    const database = client.db("dataExpress");
+    const collection = database.collection("users");
 
-    try {
-      await client.connect();
-      const database = client.db("dataExpress");
-      const collection = database.collection("users");
-      collection.find({}, function(err, docs){
-        if (err) {
-            console.log(err);
-            return;
-        }
-        docs.forEach(function(doc, index) { 
-            let person = collection.findOne(doc) 
-            console.log(person) 
-        });
+    const stats = [
+      {
+        question: "Favorite Cookies",
+        options: ["Chips Ahoy", "Oreo", "Oatmeal Raisin", "Macadamia Nut"],
+        votes: [0, 0, 0, 0]
+      },
+      {
+        question: "Favorite Color",
+        options: ["Red", "Green", "Blue", "Purple"],
+        votes: [0, 0, 0, 0]
+      },
+      {
+        question: "Favorite Programming Language",
+        options: ["Java", "JavaScript", "Python", "C++"],
+        votes: [0, 0, 0, 0]
+      }
+    ]
+
+    const users = await collection.find({}).toArray();
+    users.forEach(user => {
+      stats[0].votes[stats[0].options.indexOf(user.answer1)]++;
+      stats[1].votes[stats[1].options.indexOf(user.answer2)]++;
+      stats[2].votes[stats[2].options.indexOf(user.answer3)]++;
     });
-
-
-      res.json({ success: true });
-    } catch (err) {
-      console.log(err);
-      res.json({ success: false });
-    } finally {
-      await client.close();
-    }
-
+    res.json(stats);
+  } catch (err) {
+    console.log(err);
+    res.json({ success: false });
+  } finally {
+    await client.close();
+  }
 });
 
 router.post('/', function(req, res) { // sign up
